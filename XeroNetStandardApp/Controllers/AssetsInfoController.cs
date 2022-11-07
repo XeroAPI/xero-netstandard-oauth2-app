42338e94beb06c44b14075ca73e6f6da91b47c12
Jason Rising <jason.rising@xero.com>
2022-11-07 13:22:31 +1100

    Refactored Assets controller

--- XeroNetStandardApp/Controllers/AssetsInfoController.cs
+++ XeroNetStandardApp/Controllers/AssetsInfoController.cs
@@ -1,112 +1,87 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Xero.NetStandard.OAuth2.Model.Asset;
-using Xero.NetStandard.OAuth2.Token;
 using Xero.NetStandard.OAuth2.Api;
 using Xero.NetStandard.OAuth2.Config;
-using Xero.NetStandard.OAuth2.Client;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Net.Http;
 
 namespace XeroNetStandardApp.Controllers
 {
-  public class AssetsInfo : Controller
-  {
-    private readonly ILogger<AuthorizationController> _logger;
-    private readonly IOptions<XeroConfiguration> XeroConfig;
-
-    public AssetsInfo(IOptions<XeroConfiguration> XeroConfig, ILogger<AuthorizationController> logger)
+    /// <summary>
+    /// Controller implementing methods demonstrating following asset endpoints:
+    /// <para>- GET: /Assets</para>
+    /// </summary>
+    public class AssetsInfo : Controller
     {
-      _logger = logger;
-      this.XeroConfig = XeroConfig;
+        private readonly IOptions<XeroConfiguration> _xeroConfig;
+        private readonly AssetApi _assetApi;
+
+        public AssetsInfo(IOptions<XeroConfiguration> xeroConfig)
+        {
+            _xeroConfig = xeroConfig;
+            _assetApi = new AssetApi();
+        }
+
+        #region GET Endpoints
+        
+        /// <summary>
+        /// GET: /Assets/
+        /// </summary>
+        /// <returns>Returns list of assets</returns>
+        public async Task<ActionResult> Index()
+        {
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
+
+            // Call get assets endpoint
+            var response = await _assetApi.GetAssetsAsync(xeroToken.AccessToken, xeroTenantId, AssetStatusQueryParam.DRAFT);
+
+            ViewBag.jsonResponse = response.ToJson();
+            return View(response.Items);
+        }
+
+        /// <summary>
+        /// GET: /AssetsInfo#Create
+        /// <para>Helper method to return View</para>
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region POST Endpoints
+
+        /// <summary>
+        /// POST: /AssetsInfo#Create
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="number"></param>
+        /// <returns>Returns action result redirecting user to get assets page</returns>
+        [HttpPost]
+        public async Task<ActionResult> Create(string name, string number)
+        {
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
+
+            // Construct asset object
+            var asset = new Asset
+            {
+                AssetName = name,
+                AssetNumber = number
+            };
+
+            // Call create asset endpoint
+            await _assetApi.CreateAssetAsync(xeroToken.AccessToken, xeroTenantId, asset);
+
+            return RedirectToAction("Index", "AssetsInfo");
+        }
+
+        #endregion
     }
-
-    // GET: /Assets/
-    public async Task<ActionResult> Index()
-    {
-      var xeroToken = TokenUtilities.GetStoredToken();
-      var utcTimeNow = DateTime.UtcNow;
-
-      if (utcTimeNow > xeroToken.ExpiresAtUtc)
-      {
-        var client = new XeroClient(XeroConfig.Value);
-        xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-        TokenUtilities.StoreToken(xeroToken);
-      }
-
-      string accessToken = xeroToken.AccessToken;
-      Guid tenantId = TokenUtilities.GetCurrentTenantId();
-      string xeroTenantId;
-      if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-      {
-        xeroTenantId = tenantId.ToString();
-      }
-      else
-      {
-        var id = xeroToken.Tenants.First().TenantId;
-        xeroTenantId = id.ToString();
-        TokenUtilities.StoreTenantId(id);
-      }
-
-      var AssetApi = new AssetApi();
-
-      var response = await AssetApi.GetAssetsAsync(accessToken, xeroTenantId, AssetStatusQueryParam.DRAFT);
-
-      var assetItems = response.Items;
-
-      ViewBag.jsonResponse = response.ToJson();
-
-      return View(assetItems);
-    }
-
-    // GET: /AssetsInfo#Create
-    [HttpGet]
-    public IActionResult Create()
-    {
-      return View();
-    }
-
-    // POST: /AssetsInfo#Create
-    [HttpPost]
-    public async Task<ActionResult> Create(string Name, string Number)
-    {
-      var xeroToken = TokenUtilities.GetStoredToken();
-      var utcTimeNow = DateTime.UtcNow;
-
-      if (utcTimeNow > xeroToken.ExpiresAtUtc)
-      {
-        var client = new XeroClient(XeroConfig.Value);
-        xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-        TokenUtilities.StoreToken(xeroToken);
-      }
-
-      string accessToken = xeroToken.AccessToken;
-      Guid tenantId = TokenUtilities.GetCurrentTenantId();
-      string xeroTenantId;
-      if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-      {
-        xeroTenantId = tenantId.ToString();
-      }
-      else
-      {
-        var id = xeroToken.Tenants.First().TenantId;
-        xeroTenantId = id.ToString();
-        TokenUtilities.StoreTenantId(id);
-      }
-
-      var asset = new Asset() {
-        AssetName = Name,
-        AssetNumber = Number
-      };
-
-      var AssetApi = new AssetApi();
-      var response = await AssetApi.CreateAssetAsync(accessToken, xeroTenantId, asset);
-
-      return RedirectToAction("Index", "AssetsInfo");
-    }
-  }
 }
