9d51e612c99736e31c03681a8010bd29fc88d046
Jason Rising <jason.rising@xero.com>
2022-11-07 13:07:35 +1100

    Removed redundant code

--- XeroNetStandardApp/Controllers/AccountsInfoController.cs
+++ XeroNetStandardApp/Controllers/AccountsInfoController.cs
@@ -1,62 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Xero.NetStandard.OAuth2.Api;
-using Xero.NetStandard.OAuth2.Client;
 using Xero.NetStandard.OAuth2.Config;
-using Xero.NetStandard.OAuth2.Token;
 
 namespace XeroNetStandardApp.Controllers
 {
+    /// <summary>
+    /// Controller supporting get accounts endpoint
+    /// </summary>
     public class AccountsInfoController : Controller
     {
+        private readonly IOptions<XeroConfiguration> _xeroConfig;
+        private readonly AccountingApi _accountingApi;
 
-        private readonly ILogger<AccountsInfoController> _logger;
-        private readonly IOptions<XeroConfiguration> XeroConfig;
-
-        public AccountsInfoController(IOptions<XeroConfiguration> XeroConfig, ILogger<AccountsInfoController> logger)
+        public AccountsInfoController(IOptions<XeroConfiguration> xeroConfig)
         {
-            _logger = logger;
-            this.XeroConfig = XeroConfig;
+            _xeroConfig = xeroConfig;
+            _accountingApi = new AccountingApi();
         }
+
+        /// <summary>
+        /// GET: /GetAccounts
+        /// </summary>
+        /// <returns>Returns list of accounts</returns>
         public async Task<IActionResult> Index()
         {
-            var xeroToken = TokenUtilities.GetStoredToken();
-            var utcTimeNow = DateTime.UtcNow;
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
 
-            if (utcTimeNow > xeroToken.ExpiresAtUtc)
-            {
-                var client = new XeroClient(XeroConfig.Value);
-                xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-                TokenUtilities.StoreToken(xeroToken);
-            }
-
-            string accessToken = xeroToken.AccessToken;
-            Guid tenantId = TokenUtilities.GetCurrentTenantId();
-            string xeroTenantId;
-            if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-            {
-                xeroTenantId = tenantId.ToString();
-            }
-            else
-            {
-                var id = xeroToken.Tenants.First().TenantId;
-                xeroTenantId = id.ToString();
-                TokenUtilities.StoreTenantId(id);
-            }
-
-            var AccountingApi = new AccountingApi();
-
-            var currencyResponse = await AccountingApi.GetCurrenciesAsync(accessToken, xeroTenantId);
-            
-            var response = await AccountingApi.GetAccountsAsync(accessToken, xeroTenantId);
+            var response = await _accountingApi.GetAccountsAsync(xeroToken.AccessToken, xeroTenantId);
 
             ViewBag.jsonResponse = response.ToJson();
 
-            return View(response._Accounts);
+            // Limit response to max of 10 accounts in display
+            return View(response._Accounts.Take(10));
         }
     }
 }
