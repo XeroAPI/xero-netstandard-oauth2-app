28a3252d80dbe5c1e9c9ca8280e0457e3623dd1f
Jason Rising <jason.rising@xero.com>
2022-11-08 14:38:01 +1100

    Refactored bank transaction controller

--- XeroNetStandardApp/Controllers/Accounting/BankTransactionsInfoController.cs
+++ XeroNetStandardApp/Controllers/Accounting/BankTransactionsInfoController.cs
@@ -1,63 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Threading.Tasks;
-using Xero.NetStandard.OAuth2.Token;
 using Xero.NetStandard.OAuth2.Api;
 using Xero.NetStandard.OAuth2.Config;
-using Xero.NetStandard.OAuth2.Client;
-using Xero.NetStandard.OAuth2.Model.Accounting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace XeroNetStandardApp.Controllers
 {
+    /// <summary>
+    /// Controller implementing methods demonstrating following accounting endpoints:
+    /// <para>- GET: /BankTransactionsInfo/</para>
+    /// </summary>
     public class BankTransactionsInfo : Controller
     {
+        private readonly IOptions<XeroConfiguration> _xeroConfig;
+        private readonly AccountingApi _accountingApi;
 
-        private readonly ILogger<AuthorizationController> _logger;
-        private readonly IOptions<XeroConfiguration> XeroConfig;
-
-        public BankTransactionsInfo(IOptions<XeroConfiguration> XeroConfig, ILogger<AuthorizationController> logger)
+        public BankTransactionsInfo(IOptions<XeroConfiguration> xeroConfig)
         {
-            _logger = logger;
-            this.XeroConfig = XeroConfig;
+           _xeroConfig = xeroConfig;
+           _accountingApi = new AccountingApi();
         }
 
-        // GET: /BankTransactionsInfo/
+        /// <summary>
+        /// GET: /BankTransactionsInfo/
+        /// </summary>
+        /// <returns>Returns list of bank transactions</returns>
         public async Task<ActionResult> Index()
         {
-            var xeroToken = TokenUtilities.GetStoredToken();
-            var utcTimeNow = DateTime.UtcNow;
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
 
-            if (utcTimeNow > xeroToken.ExpiresAtUtc)
-            {
-                var client = new XeroClient(XeroConfig.Value);
-                xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-                TokenUtilities.StoreToken(xeroToken);
-            }
+            // Call get bank transaction endpoint
+            var response = await _accountingApi.GetBankTransactionsAsync(xeroToken.AccessToken, xeroTenantId);
 
-            string accessToken = xeroToken.AccessToken;
-            Guid tenantId = TokenUtilities.GetCurrentTenantId();
-            string xeroTenantId;
-            if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-            {
-                xeroTenantId = tenantId.ToString();
-            }
-            else
-            {
-                var id = xeroToken.Tenants.First().TenantId;
-                xeroTenantId = id.ToString();
-                TokenUtilities.StoreTenantId(id);
-            }
-
-            var AccountingApi = new AccountingApi();
-            var response = await AccountingApi.GetBankTransactionsAsync(accessToken, xeroTenantId);
-            var bankTransactions = response._BankTransactions;
             ViewBag.jsonResponse = response.ToJson();
-
-            return View(bankTransactions);
+            return View(response._BankTransactions);
         }
 
     }
