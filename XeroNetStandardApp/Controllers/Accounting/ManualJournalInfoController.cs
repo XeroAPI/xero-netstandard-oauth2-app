afd24599d150d979b08aa1193727157e8d22e362
Jason Rising <jason.rising@xero.com>
2022-11-09 11:58:24 +1100

    Refactored ManualJournalInfo controller

--- XeroNetStandardApp/Controllers/Accounting/ManualJournalInfoController.cs
+++ XeroNetStandardApp/Controllers/Accounting/ManualJournalInfoController.cs
@@ -1,198 +1,136 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Xero.NetStandard.OAuth2.Api;
-using Xero.NetStandard.OAuth2.Client;
 using Xero.NetStandard.OAuth2.Config;
 using Xero.NetStandard.OAuth2.Model.Accounting;
-using Xero.NetStandard.OAuth2.Token;
 
 namespace XeroNetStandardApp.Controllers
 {
+    /// <summary>
+    /// Controller implementing methods demonstrating following accounting endpoints:
+    /// <para>- GET: /ManualJournalInfo/</para>
+    /// <para>- POST: /ManualJournalInfo#Create</para>
+    /// </summary>
     public class ManualJournalInfo : Controller
     {
-        private readonly ILogger<ManualJournalInfo> _logger;
-        private readonly IOptions<XeroConfiguration> XeroConfig;
-        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IOptions<XeroConfiguration> _xeroConfig;
+        private readonly AccountingApi _accountingApi;
 
-        public ManualJournalInfo(IOptions<XeroConfiguration> XeroConfig, IHttpClientFactory httpClientFactory, ILogger<ManualJournalInfo> logger)
+        public ManualJournalInfo(IOptions<XeroConfiguration> xeroConfig)
         {
-            _logger = logger;
-            this.XeroConfig = XeroConfig;
-            this.httpClientFactory = httpClientFactory;
+            _xeroConfig = xeroConfig;
+            _accountingApi = new AccountingApi();
         }
 
-        // GET: /ManualJournalInfo/
+        #region GET Endpoints
+
+        /// <summary>
+        /// GET: /ManualJournalInfo/
+        /// </summary>
+        /// <returns>Returns a list of manual journals</returns>
         public async Task<ActionResult> Index()
         {
-            // Authentication
-            var xeroToken = TokenUtilities.GetStoredToken();
-            var utcTimeNow = DateTime.UtcNow;
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
 
-            if (utcTimeNow > xeroToken.ExpiresAtUtc)
-            {
-                var client = new XeroClient(XeroConfig.Value);
-                xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-                TokenUtilities.StoreToken(xeroToken);
-            }
-
-            string accessToken = xeroToken.AccessToken;
-            Guid tenantId = TokenUtilities.GetCurrentTenantId();
-            string xeroTenantId;
-            if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-            {
-                xeroTenantId = tenantId.ToString();
-            }
-            else
-            {
-                var id = xeroToken.Tenants.First().TenantId;
-                xeroTenantId = id.ToString();
-                TokenUtilities.StoreTenantId(id);
-            }
-            var AccountingApi = new AccountingApi();
-
-            var response = await AccountingApi.GetManualJournalsAsync(accessToken, xeroTenantId, null, null);
+            // Call get manual journals endpoint
+            var response = await _accountingApi.GetManualJournalsAsync(xeroToken.AccessToken, xeroTenantId);
 
             ViewBag.jsonResponse = response.ToJson();
-
             return View(response._ManualJournals);
         }
 
+        /// <summary>
+        /// GET: /ManualJournalInfo/#manualJournalId
+        /// </summary>
+        /// <param name="manualJournalId">Id of a specific manual journal to get</param>
+        /// <returns>Returns a specific journal specified by provided id if exists</returns>
         [HttpGet]
         public async Task<IActionResult> FindJournal(Guid manualJournalId)
         {
-            // Authentication
-            var xeroToken = TokenUtilities.GetStoredToken();
-            var utcTimeNow = DateTime.UtcNow;
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
 
-            if (utcTimeNow > xeroToken.ExpiresAtUtc)
-            {
-                var client = new XeroClient(XeroConfig.Value);
-                xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-                TokenUtilities.StoreToken(xeroToken);
-            }
-
-            string accessToken = xeroToken.AccessToken;
-            Guid tenantId = TokenUtilities.GetCurrentTenantId();
-            string xeroTenantId;
-            if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-            {
-                xeroTenantId = tenantId.ToString();
-            }
-            else
-            {
-                var id = xeroToken.Tenants.First().TenantId;
-                xeroTenantId = id.ToString();
-                TokenUtilities.StoreTenantId(id);
-            }
-            var AccountingApi = new AccountingApi();
-
-            var response = await AccountingApi.GetManualJournalAsync(accessToken, xeroTenantId, manualJournalId);
+            // Call get manual journal for a specific journal
+            var response = await _accountingApi.GetManualJournalAsync(xeroToken.AccessToken, xeroTenantId, manualJournalId);
 
             ViewBag.jsonResponse = response.ToJson();
-
             return View(response._ManualJournals.First());
         }
 
 
-        // GET: /ManualJournalInfo#Create
+        /// <summary>
+        /// GET: /ManualJournalInfo#Create
+        /// <para>Helper method to return View</para>
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: /ManualJournalInfo#Create
+        #endregion
+
+        #region POST Endpoints
+
+
+        /// <summary>
+        /// POST: /ManualJournalInfo#Create
+        /// </summary>
+        /// <param name="narration">Narration of manual journal to create</param>
+        /// <param name="taxType">Tax type of manual journal lines to create</param>
+        /// <returns>Returns action result to redirect user to get journals page</returns>
         [HttpPost]
         public async Task<ActionResult> Create(string narration, string taxType)
         {
-            // Authentication
-            var xeroToken = TokenUtilities.GetStoredToken();
-            var utcTimeNow = DateTime.UtcNow;
+            // Token and TenantId setup
+            var xeroToken = await TokenUtilities.GetXeroOAuth2Token(_xeroConfig.Value);
+            var xeroTenantId = TokenUtilities.GetXeroTenantId(xeroToken);
 
-            if (utcTimeNow > xeroToken.ExpiresAtUtc)
+            // Construct manual journals object
+            // Manual journals must contain at least two journal lines
+            var manualJournalLines = new List<ManualJournalLine>
             {
-                var client = new XeroClient(XeroConfig.Value);
-                xeroToken = (XeroOAuth2Token)await client.RefreshAccessTokenAsync(xeroToken);
-                TokenUtilities.StoreToken(xeroToken);
-            }
-
-            string accessToken = xeroToken.AccessToken;
-            Guid tenantId = TokenUtilities.GetCurrentTenantId();
-            string xeroTenantId;
-            if (xeroToken.Tenants.Any((t) => t.TenantId == tenantId))
-            {
-                xeroTenantId = tenantId.ToString();
-            }
-            else
-            {
-                var id = xeroToken.Tenants.First().TenantId;
-                xeroTenantId = id.ToString();
-                TokenUtilities.StoreTenantId(id);
-            }
-
-            // Creates two journal lines
-            var manualJournalLines = new List<ManualJournalLine>();
-
-            var credit = new ManualJournalLine
-            {
-
-                LineAmount = new decimal(100.0),
-                AccountCode = "400",
-                Description = "Credit",
-                Tracking = new List<TrackingCategory>(),
-                TaxType = taxType
-            };
-            manualJournalLines.Add(credit);
-
-            var debit = new ManualJournalLine
-            {
-                LineAmount = new decimal(-100.0),
-                AccountCode = "120",
-                Description = "Debit",
-                Tracking = new List<TrackingCategory>(),
-                TaxType = taxType
-            };
-            manualJournalLines.Add(debit);
-
-            var AccountingApi = new AccountingApi();
-
-            // Picks first tracking category and option if available, and attaches to journal line
-            var trackingCategoriesResponse = await AccountingApi.GetTrackingCategoriesAsync(accessToken, xeroTenantId, null, null);
-            if (trackingCategoriesResponse._TrackingCategories.Count != 0 && trackingCategoriesResponse._TrackingCategories.First().Options.First().TrackingOptionID != null)
-            {
-                var trackingCategory = new TrackingCategory
+                new ManualJournalLine
                 {
-                    TrackingCategoryID = trackingCategoriesResponse._TrackingCategories.First().TrackingCategoryID,
-                    TrackingOptionID = trackingCategoriesResponse._TrackingCategories.First().Options.First().TrackingOptionID,
-                    Name = trackingCategoriesResponse._TrackingCategories.First().Name,
-                    Status = trackingCategoriesResponse._TrackingCategories.First().Status
-                };
-                credit.Tracking.Add(trackingCategory);
-                debit.Tracking.Add(trackingCategory);
-            }
-
-            // Create manual journals
-            ManualJournal manualJournal = new ManualJournal
-            {
-                Narration = narration,
-                JournalLines = manualJournalLines,
-                Date = DateTime.Today
+                    LineAmount = 100,
+                    AccountCode = "400",
+                    TaxType = taxType
+                }, 
+                new ManualJournalLine
+                {
+                    LineAmount = -100,
+                    AccountCode = "120",
+                    TaxType = taxType
+                }
             };
 
-            var manualJournals = new ManualJournals();
-            var manualJournalsList = new List<ManualJournal>();
-            manualJournalsList.Add(manualJournal);
-            manualJournals._ManualJournals = manualJournalsList;
+            var manualJournals = new ManualJournals
+            {
+                _ManualJournals = new List<ManualJournal>
+                {
+                    new ManualJournal
+                    {
+                        Narration = narration,
+                        JournalLines = manualJournalLines,
+                        Date = DateTime.Today
+                    }
+                }
+            };
 
-            await AccountingApi.CreateManualJournalsAsync(accessToken, xeroTenantId, manualJournals, null);
+            // Call create manual journal endpoint
+            await _accountingApi.CreateManualJournalsAsync(xeroToken.AccessToken, xeroTenantId, manualJournals);
 
             return RedirectToAction("Index", "ManualJournalInfo");
         }
+
+        #endregion
     }
 }
